caa678305c9435e7829bf9f6686b1e7a922b1503
wkd3475 <wkd3475@gmail.com>
2022-03-27 23:45:08 +0900

    Vue init

---
+++ vue_front/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <MainLayout />
+  </div>
+</template>
+
+<script>
+import MainLayout from './components/Layout/MainLayout.vue'
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+  }
+}
+</script>
